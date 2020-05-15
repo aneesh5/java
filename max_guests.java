d28be054e522786c524b91f0d4529b8b0c5f41da
Aneesh <37624818+aneesh5@users.noreply.github.com>
2020-05-15 15:53:33 +0530

    Add files via upload
    Consider a big party where a log register for guest’s entry and exit times is maintained. Find the time at which there are maximum guests in the party. Note that entries in register are not in any order.

---
+++ max_guests.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+class max_guests{
+	static int find(int arr[],int exit[],int n){
+		int i=0,j=0,index=-1,g=0,max=0;
+		Arrays.sort(arr);
+		Arrays.sort(exit);
+		while(i<n){
+			if(arr[i]>=exit[j]){
+				g=g-1;
+				j++;
+			}
+			else{
+				g=g+1;
+				i++;
+			}
+			if(g>max){
+				max=g;
+				index=i-1;
+			}
+		}
+		return arr[index];
+	}
+
+	public static void main(String args[]){
+		int arrl[] = {13, 28, 29, 14, 40, 17, 3 }; 
+        int exit[] = {107, 95, 111, 105, 70, 127, 74}; 
+        int n = arrl.length; 
+        System.out.println(find(arrl, exit, n)); 
+	}
+}
