b15024757a04b290dd3ade978231a01d1a4ecd99
Aneesh <37624818+aneesh5@users.noreply.github.com>
2020-05-15 15:58:00 +0530

    Add files via upload

---
+++ majority.java
@@ -0,0 +1,26 @@
+class majority{
+	static int majo(int arr[],int len){
+		//int len=arr.length;
+		int max=0,index=-1;
+		for(int i=0;i<len;i++){
+			int c=0;
+			for(int j=0;j<len;j++){
+				if(arr[i]==arr[j]){
+					c=c+1;
+				}
+
+			}
+			if(c>max){
+				max=c;
+				index=i;
+			}
+		}
+		return arr[index];
+	}
+
+	public static void main(String args[]){
+		int arr[]={1, 2, 2, 2, 5, 5, 5};
+		int len=arr.length;
+		System.out.println(majo(arr,len));
+	}
+}
